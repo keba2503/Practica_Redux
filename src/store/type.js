b3dc27fff979532ffb5adcc138a3b249ea7c1fbc
Karen Borrero <keba2503@gmail.com>
2020-06-25 11:36:47 +0100

    firts commit

---
+++ src/store/type.js
@@ -0,0 +1,14 @@
+// crear las bicis en el store
+export const SET_BIKES = 'SET_BIKES';
+
+//Ejecutar el filtro
+export const SET_FILTERS = 'SET-FILTERS';
+
+//Añadir una bici al carrito de la compra
+export const ADD_TO_CART = 'ADD_TO_CART';
+
+//ELIMINAR UNA BICI DEL CARRITO
+export const REMOVE_TO_CART 'REMOVE_TO_CART';
+
+// Chequear el carrito
+export const CKECKOUT_CART = 'CHECKOUT_CART';
