d21986cfdf935702ddf4dba372e34f8328d42142
Karen Borrero <keba2503@gmail.com>
2020-06-26 10:29:44 +0100

    import provider

--- src/index.js
+++ src/index.js
@@ -5,23 +5,8 @@ import Root from './components/Root';
 import './index.css';
 
 import { configureStore } from './store';
-import BikesService from './services/Bikes';
-import { 
-    setBikes, 
-    setFilter, 
-    addToCart, 
-    removeFromCart, 
-    ckeckoutCart 
-} from './store/actions';
-import {BIKE_FILTERS} from './constants'
+
 
 const store = configureStore()
-store.dispatch(setBikes(BikesService.getAllBikes()));
-store.dispatch(setFilter(BIKE_FILTERS.MOUNTAIN));
-store.dispatch(addToCart('1', 2));
-store.dispatch(removeFromCart('1', 2));
-store.dispatch(addToCart('2', 3));
-store.dispatch(addToCart('2', 1));
-store.dispatch(ckeckoutCart())
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
